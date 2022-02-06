3a197f8e3a7eebe7abf17c70a3feacc9b04a5e7f
claudioed <claudioed.oliveira@gmail.com>
2022-02-06 10:18:18 -0300

    add env variable for elastic

--- internal/infra/elastic/elastic_producer.go
+++ internal/infra/elastic/elastic_producer.go
@@ -7,8 +7,9 @@ import (
 
 func NewESClient() (*elasticsearch7.Client, error) {
 	es7, err := elasticsearch7.NewClient(elasticsearch7.Config{
-		Username: os.Getenv("ELASTICSEARCH_USERNAME"),
-		Password: os.Getenv("ELASTICSEARCH_PASSWORD"),
+		Username:  os.Getenv("ELASTICSEARCH_USERNAME"),
+		Password:  os.Getenv("ELASTICSEARCH_PASSWORD"),
+		Addresses: []string{os.Getenv("ELASTICSEARCH_ADDRESSES")},
 	})
 	return es7, err
 }
